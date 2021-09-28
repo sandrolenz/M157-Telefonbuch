314b754e0d9818dea44f933cc7addde241735439
SandroLenz <59011553+SandroLenz@users.noreply.github.com>
2021-09-28 10:14:50 +0200

    Small changes
    - Color
    - Search icon
    - Imprint

--- index.php
+++ index.php
@@ -23,8 +23,8 @@
             <br><br><br>
             <div class="div_result">
                 <h1 id="txt_name">Vorname Nachname</h1><br>
-                <img id="img_person" src="./img/avatar_placeholder.jpg"> <!-- https://thispersondoesnotexist.com/image -->
-                <br><a id="separator">–––––––––/–––––––––</a>
+                <!-- <img id="img_person" src="./img/avatar_placeholder.jpg">  https://thispersondoesnotexist.com/image -->
+                <br><a id="separator">–––––––––/–––––––––</a><br>
                 <p id="txt_email">E-Mail</p>
                 <p id="txt_phone">Telefonnummer</p>
                 <p id="txt_postiton">Position</p>
